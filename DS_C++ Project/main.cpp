98499451901ebda83e6e310fe7ceb7da0748d796
Debbie <debbie8839@gmail.com>
2020-02-21 19:33:46 +0800

    Initial Commit

---
+++ DS_C++ Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DS_C++ Project
+//
+//  Created by Debbie on 2020/2/21.
+//  Copyright © 2020 Debbie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
