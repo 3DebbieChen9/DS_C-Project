49ffdbc9139c5c20cb3af8ed0dee91f30224ac99
Debbie <debbie8839@gmail.com>
2020-04-17 17:47:30 +0800

    Add new file (COVID-19 Simulation.cpp)

---
+++ DS_C++ Project/HWs/12743 - Quiz2_COVID-19 Simulation.cpp
@@ -0,0 +1,11 @@
+//
+//  12743 - Quiz2_COVID-19 Simulation.cpp
+//  DS_C++ Project
+//
+//  Created by Debbie on 2020/4/17.
+//  Copyright © 2020 Debbie. All rights reserved.
+//
+
+#include <iostream>
+
+
